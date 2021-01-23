500bd194936c9b79ffb7f3f9592ad96274a37592
Giovanne Discacciati <gi_discacciti@hotmail.com>
2021-01-23 14:17:12 -0300

    Commit com as API's GET e POST

--- nodejs/cards/src/controllers/card-controller.js
+++ nodejs/cards/src/controllers/card-controller.js
@@ -2,6 +2,43 @@ const neDB = require('../configurations/database')
 const api = {}
 
 api.findAll = (request, response) => {
+    neDB.find({}).sort({ name: 1 }).exec((exception, cards) => {
+        if (exception) {
+            const setence = 'Oops... Could not list all cards!'
+            console.log(setence, exception)
+            response.status(exception.status | 400)
+            response.json({ 'mensagem': setence })
+        }
+        response.json(cards)
+    })
 }
+
+api.save = (request, response) => {
+    const canonical = request.body
+    neDB.insert(canonical, (exception, card) => {
+        if (exception) {
+            const setence = 'Oops... Could not list all cards!'
+            console.log(setence, exception)
+            response.status(exception.status | 400)
+            response.json({ 'mensagem': setence })
+        }
+        response.status(201)
+        response.json(card)
+    })
+}
+
+/*api.update = (request, response) =>{
+    const canonical = request.body
+    neDB.update({canonical, canonical, (exception, card) => {
+        if (exception){
+            const setence = 'Oops... Could not list all cards!'
+            console.log(setence, exception)
+            response.status(exception.status | 400)
+            response.json({ 'mensagem': setence })
+        }
+        response.status(202)
+        response.json(card)
+    })
+}*/
 
 module.exports = api
--- nodejs/cards/src/routes/card-route.js
+++ nodejs/cards/src/routes/card-route.js
@@ -1,7 +1,9 @@
 const api = require('../controllers/card-controller')
 
 module.exports = (app) => {
-    app.route('')
+    app.route('/cards')
         .get(api.findAll)
+        .post(api.save)
+        //.put(api.update)
 
 }
